7c3b3bc12df296466c84bd928ae79f23d0f6dd14
anurag <anuragrawat908@gmail.com>
2022-02-15 23:32:59 +0530

    removed warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
-    static propTypes = {};
+    // static propTypes = {};
 
     render() {
         return <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">ReadIt</a>
+                    <a className="navbar-brand" href="/">ReadIt</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
