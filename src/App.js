971631a1b83b9cb3c64db4c6790b2b9795f96e18
anurag <anuragrawat908@gmail.com>
2022-08-31 16:05:34 +0530

    TypoErro in business category resolved

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
             <Route exact path="/sports" element={<News key={'sports'} pageSize={this.pageSize} category={'sports'}/>}/> 
             <Route exact path="/science" element={<News key={'science'} pageSize={this.pageSize} category={'science'}/>}/> 
             <Route exact path="/entertainment" element={<News key={'entertainment'} pageSize={this.pageSize} category={'entertainment'}/>}/> 
-            <Route exact path="/buisness" element={<News key={'buisness'} pageSize={this.pageSize} category={'buisness'}/>}/> 
+            <Route exact path="/business" element={<News key={'business'} pageSize={this.pageSize} category={'business'}/>}/> 
             <Route exact path="/health" element={<News key={'health'} pageSize={this.pageSize} category={'health'}/>}/> 
             <Route exact path="/technology" element={<News key={'technology'} pageSize={this.pageSize} category={'technology'}/>}/> 
           </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export class Navbar extends Component {
         return <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">ReadIt</a>
+                    <Link className="navbar-brand" to="/">ReadIt</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -34,7 +34,7 @@ export class Navbar extends Component {
                                 <Link className="nav-link" to="/health">Health</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/buisness">Buisness</Link>
+                                <Link className="nav-link" to="/business">Business</Link>
                             </li>
                         </ul>
                     </div>
